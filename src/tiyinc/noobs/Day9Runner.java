848d3a88884b165da2ff398771bf8855e6a9b694
Richard Blanch <Brice@Brices-Stash.local>
2016-08-18 11:03:56 -0400

    Base Version of Day9Runner

---
+++ src/tiyinc/noobs/Day9Runner.java
@@ -0,0 +1,11 @@
+package tiyinc.noobs;
+
+/**
+ * Created by Brice on 8/18/16.
+ */
+public class Day9Runner {
+
+    public static void main(String[] args) {
+        System.out.println("Running ...");
+    }
+}
